df2b48fcab313a97097f8eb7717fe71117df0699
Vivek Parihar <vivek@yogurtlabs.co>
2015-03-13 01:30:50 +0530

    TableView generated through the code. Listed 5 list

--- MyTodo/ToDoItem.swift
+++ MyTodo/ToDoItem.swift
@@ -14,5 +14,11 @@ class ToDoItem: NSManagedObject {
     @NSManaged var todoText: String
     @NSManaged var title: String
     
-
+    class func createInManagedObjectContext(moc: NSManagedObjectContext, title: String, text: String) -> ToDoItem {
+        let newItem = NSEntityDescription.insertNewObjectForEntityForName("ToDoItem", inManagedObjectContext: moc) as ToDoItem
+        newItem.title = title
+        newItem.todoText = text
+        
+        return newItem
+    }
 }
--- MyTodo/ViewController.swift
+++ MyTodo/ViewController.swift
@@ -9,42 +9,76 @@
 import UIKit
 import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
     
     let managedObjectContext = (UIApplication.sharedApplication().delegate as AppDelegate).managedObjectContext
 
+    var todoTableView = UITableView(frame: CGRectZero, style: .Plain)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        let newTodo = NSEntityDescription.insertNewObjectForEntityForName("ToDoItem", inManagedObjectContext: self.managedObjectContext!) as ToDoItem
+//        let newTodo = NSEntityDescription.insertNewObjectForEntityForName("ToDoItem", inManagedObjectContext: self.managedObjectContext!) as ToDoItem
+//        
+//        newTodo.title = "Testing core data to write to do app"
+//        newTodo.todoText = "This is first todo entry for my app. Just make to do app in which we can enter to do"
+//        println(managedObjectContext)
+//        println(newTodo.title)
         
-        newTodo.title = "Testing core data to write to do app"
-        newTodo.todoText = "This is first todo entry for my app. Just make to do app in which we can enter to do"
-        println(managedObjectContext)
-        println(newTodo.title)
+        if let moc = managedObjectContext {
+            var items = [
+                ("Industry" , "Procution"),
+                ("Medium" , "Video"),
+                ("Tagert Audience" , "Cocnsumer+Small Bussinesses"),
+                ("Targeted Geographic" , "India")
+            ]
+            for (todoTitle, todoText) in items {
+                ToDoItem.createInManagedObjectContext(moc, title: todoTitle, text: todoText)
+            }
+        }
+        
+        var viewFrame = self.view.frame
+        viewFrame.origin.y += 20
+        viewFrame.size.height -= 20
+        todoTableView.frame = viewFrame
+        self.view.addSubview(todoTableView)
+        
+        todoTableView.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: "ToDoCell")
+        todoTableView.dataSource = self
+        
     }
     
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        //Create a new fetch request using ToDoitem netity
-        let fetchRequest = NSFetchRequest(entityName: "ToDoItem")
-        
-        //Execute the fetc request, and caste the fetch result to an array of ToDoitem objects
-        if let fetchResults = managedObjectContext!.executeFetchRequest(fetchRequest, error: nil) as? [ToDoItem] {
-            
-            //create an alert and set it's msg to whatever todoText is
-            let alert = UIAlertController(title: fetchResults[0].title,
-                message: fetchResults[0].todoText,
-                preferredStyle: .Alert)
-            
-            //Display alert
-            self.presentViewController(alert,
-                animated: true,
-                completion: nil)
-        }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ToDoCell") as UITableViewCell
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+    
+    
+//    override func viewDidAppear(animated: Bool) {
+//        super.viewDidAppear(animated)
+//        
+//        //Create a new fetch request using ToDoitem netity
+//        let fetchRequest = NSFetchRequest(entityName: "ToDoItem")
+//        
+//        //Execute the fetc request, and caste the fetch result to an array of ToDoitem objects
+//        if let fetchResults = managedObjectContext!.executeFetchRequest(fetchRequest, error: nil) as? [ToDoItem] {
+//            
+//            //create an alert and set it's msg to whatever todoText is
+//            let alert = UIAlertController(title: fetchResults[0].title,
+//                message: fetchResults[0].todoText,
+//                preferredStyle: .Alert)
+//            
+//            //Display alert
+//            self.presentViewController(alert,
+//                animated: true,
+//                completion: nil)
+//        }
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
