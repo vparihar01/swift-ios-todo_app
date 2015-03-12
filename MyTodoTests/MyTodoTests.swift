795c37b7a45ed6212420a87c24d701584c3f1e65
Vivek Parihar <KillBill@Viveks-MacBook-Pro.local>
2015-03-12 13:22:35 +0530

    Initial Commit

---
+++ MyTodoTests/MyTodoTests.swift
@@ -0,0 +1,36 @@
+//
+//  MyTodoTests.swift
+//  MyTodoTests
+//
+//  Created by Vivek Parihar on 12/03/2015.
+//  Copyright (c) 2015 Vivek Parihar. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MyTodoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
